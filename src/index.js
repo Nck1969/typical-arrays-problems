34c62c67962fecbc6507e941ccc9d0525760f5e3
Nck1969 <56834272+Nck1969@users.noreply.github.com>
2021-03-10 00:14:31 +0300

    fix
    fix functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+  if (array == undefined || array.length == 0)
   return 0;
+  let min = array[0];
+  for( let i = 0; i < array.length; i++)
+  if(min > array[i]){
+    min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
+  if (array == undefined || array.length == 0)
   return 0;
+  let max = array[0];
+  for( let i = 0; i < array.length; i++)
+  if(max < array[i]){
+    max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
+  if (array == undefined || array.length == 0)
   return 0;
+  let rez = 0;
+  for( let i = 0; i < array.length; i++){
+   rez = rez + array[i];
+  }
+  rez = rez / array.length;
+  return rez;
 }
+
